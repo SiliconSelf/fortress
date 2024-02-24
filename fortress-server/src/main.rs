033fa9f4ed69a3ba3b5cc51b6ffada1780c17ced
Wren Morozova <siliconself@proton.me>
2024-02-24 16:23:51 -0700

    Created basic hello world for testing

--- fortress-server/src/main.rs
+++ fortress-server/src/main.rs
@@ -1,5 +1,21 @@
 #![doc = include_str!("../README.md")]
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+/// Just a basic hello world for now
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    simple_logger::init().expect("Failed to initialize logging.");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
